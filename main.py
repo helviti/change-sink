b8a1f8e19122e815449002ac10b902209bd9017a
helviti <canfurkankalaycioglu@gmail.com>
2024-10-14 20:15:38 +0300

    fixed minor bug, print actual device name

--- main.py
+++ main.py
@@ -3,52 +3,79 @@ import subprocess
 import sys
 import json
 
+
 def get_sinks():
-    cmd = subprocess.run(["pactl","--format","json","list","short","sinks"],capture_output=True)
+    cmd = subprocess.run(
+        ["pactl", "--format", "json", "list", "sinks"], capture_output=True
+    )
     if cmd.returncode != 0:
-        raise Exception(f"Failed to run command: {cmd.stdout.decode()} {cmd.stderr.decode()}")
+        raise Exception(
+            f"Failed to run command: {cmd.stdout.decode()} {cmd.stderr.decode()}"
+        )
 
     output = cmd.stdout.decode()
     jsonDecoded = json.loads(output)
 
     return jsonDecoded
 
+
 def set_default_sink(sink):
-    print(f"Setting default sink to: id={sink['index']} name={sink['name']}")
-    cmd = subprocess.run(["pactl","set-default-sink",f"{sink['index']}"],capture_output=True)
+    print(f"Setting default sink to: id={sink['index']} name={sink['properties']['device.product.name']}")
+    cmd = subprocess.run(
+        ["pactl", "set-default-sink", f"{sink['index']}"], capture_output=True
+    )
     if cmd.returncode != 0:
-        raise Exception(f"Failed to run command: {cmd.stdout.decode()} {cmd.stderr.decode()}")
+        raise Exception(
+            f"Failed to run command: {cmd.stdout.decode()} {cmd.stderr.decode()}"
+        )
+
+
+def get_default_sink_name():
+    cmd = subprocess.run(["pactl", "get-default-sink"], capture_output=True)
+    if cmd.returncode != 0:
+        raise Exception(
+            f"Failed to run command: {cmd.stdout.decode()} {cmd.stderr.decode()}"
+        )
+
+    return cmd.stdout.decode().rstrip("\n")
+
 
 def main():
     if len(sys.argv) < 2:
-        raise Exception("No mode found. Usage: python main.py (switch|show)")
+        raise Exception("No mode found. Usage: python main.py (switch|show|list)")
 
     mode = sys.argv[1]
 
     sinks = get_sinks()
-    default_sink_list = [sink for sink in sinks if sink['state'] == 'RUNNING']
-    if len(default_sink_list) == 0:
-        raise Exception('No default sink found')
+    default_sink = next(
+        filter(lambda s: s["name"] == get_default_sink_name(), sinks), None
+    )
 
-    default_sink = default_sink_list[0]
-    default_sink_id = default_sink['index']
+    if default_sink is None:
+        raise Exception("No default sink found")
 
-    if mode == 'switch':
+    if mode == "switch":
         for idx, _ in enumerate(sinks):
             # continue until default sink is found
-            if sinks[idx]['state'] != 'RUNNING':
+            if sinks[idx]["index"] != default_sink["index"]:
                 continue
-
             # no next element, switch to first
-            if idx+1 == len(sinks):
+            if idx + 1 == len(sinks):
                 set_default_sink(sinks[0])
+                return
             # next element exists, switch to it
             else:
-                set_default_sink(sinks[idx+1])
-    elif mode == 'show':
-        print(json.dumps(default_sink,indent=2))
+                set_default_sink(sinks[idx + 1])
+                return
+    elif mode == "show":
+        print(json.dumps(default_sink, indent=2))
+    elif mode == "list":
+        print(json.dumps(sinks, indent=2))
     else:
-        raise Exception(f"Unsupported mode: '{mode}'. Usage: python main.py (switch|show)")
+        raise Exception(
+            f"Unsupported mode: '{mode}'. Usage: python main.py (switch|show|list)"
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     raise SystemExit(main())
